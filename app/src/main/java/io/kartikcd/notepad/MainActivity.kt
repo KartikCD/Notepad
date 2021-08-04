f0ba8f5f84861c739794baa5a20724168f1baaaf
KartikCD <chawdakartik@gmail.com>
2021-08-04 17:00:01 +0530

    initial setup

---
+++ app/src/main/java/io/kartikcd/notepad/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.kartikcd.notepad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

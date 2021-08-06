9c661214446794e5d0be1f57308a8ede6f8c475f
KartikCD <chawdakartik@gmail.com>
2021-08-07 02:13:48 +0530

    list fragment menu added

--- app/src/main/java/io/kartikcd/notepad/presentation/ui/listnotes/ListNotesFragment.kt
+++ app/src/main/java/io/kartikcd/notepad/presentation/ui/listnotes/ListNotesFragment.kt
@@ -1,19 +1,32 @@
 package io.kartikcd.notepad.presentation.ui.listnotes
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import io.kartikcd.notepad.R
+import io.kartikcd.notepad.databinding.FragmentListNotesBinding
 
 class ListNotesFragment : Fragment() {
+
+    private var _binding: FragmentListNotesBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_notes, container, false)
+        _binding = FragmentListNotesBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
+        return _binding?.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_note_list, menu)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
